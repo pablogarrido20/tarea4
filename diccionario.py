096565a6536c1bce02512af939eb5526555c3fbc
Pablo Garrido <edarventen2@gmail.com>
2020-09-06 13:29:51 -0300

    diccionario

---
+++ diccionario.py
@@ -0,0 +1,8 @@
+diccionario = {'nombre': ['Pablo','Antonia','Diego','Moises'], 'apellido': ['Garrido','Marín','Molina', 'Axebahia'],'cursos': ['Programacion', 'fisica'], 'nota programacion': [50,34,63,40], 'nota fisica': [60,58,66,50]}
+
+print(diccionario['nombre'][0])
+print(diccionario['apellido'][0])
+print(diccionario['cursos'][0])
+print(diccionario['nota programacion'][0])
+print(diccionario['cursos'][1])
+print(diccionario['nota fisica'][0])
