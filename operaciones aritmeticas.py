07b709df8cb71161cd882f1eeab3702f864429fc
Pablo Garrido <edarventen2@gmail.com>
2020-09-06 13:13:01 -0300

    operaciones aritmeticas

---
+++ operaciones aritmeticas.py
@@ -0,0 +1,8 @@
+x= 7
+y= 4
+print(f'La suma se define como {x}+{y}={x+y}')
+print(f'La resta se define como {x}-{y}={x-y}')
+print(f'La multiplicación se define como {x}*{y}={x*y}')
+print(f'La division se define como {x}/{y}={x/y}')
+print(f'El modulo se define como el resto de la division, por lo que el denominador multiplicado por la division sumado con el resto nos da el  numerados {x}%{y}={x%y}, por lo tanto {y}* {x/y} + {x%y} ={x}')
+
